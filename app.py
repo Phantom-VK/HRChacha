00111824f1bdfb4863d63d009e68ee85dda5da4d
Phantom-VK <vikramadityakhupse@gmail.com>
2025-07-10 22:23:39 +0530

    LLM

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import sys
 
 import streamlit as st
+
 from hrchacha.constants import USER_CHAT_INPUT_KEY
 from hrchacha.exceptions.exception import HRChachaException
 from hrchacha.ui.main_window import MainWindowUI
-from hrchacha.logging.logger import logging
+
 
 def custom_response_handler(prompt: str) -> str:
     """Custom logic for generating bot responses.
@@ -20,7 +21,6 @@ def custom_response_handler(prompt: str) -> str:
 
 if __name__ == "__main__":
     try:
-        logging.info("Started Chatbot")
         main_app = MainWindowUI(
                 title=" HRChacha – Your Tech Job Buddy",
                 response_callback=custom_response_handler
